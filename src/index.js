d363678e7c358e90fa3bc1c8b852fbe9c10f854d
jheart-vic <adebowalevictorjheart.com>
2022-05-31 19:45:53 +0100

    Finish the Project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import './style.css';
 import Task from '../modules/task.js';
 
-const task = new Task ();
- task.addList( 'Dance with Sanja', true,  0)
- task.addList( 'Dance with Toma', false,  1)
- task.addList( 'Dance with Waleed', true,  )
+const task = new Task();
+task.addList('Dance with Sanja', true, 0);
+task.addList('Dance with Toma', false, 1);
+task.addList('Dance with Waleed', true);
+task.addList('Dance with max', true, 3);
 // task.addList({description: 'Dance with Sanja', completed: true, index: 1});
