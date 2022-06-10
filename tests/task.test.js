683e11b092aaa5a5f6e03b12e9afbe3aa65f147c
Debabrata Mandal <bapidebabrata@gmail.com>
2022-06-10 08:35:21 +0530

    Add test for remove task

--- tests/task.test.js
+++ tests/task.test.js
@@ -11,4 +11,16 @@ describe('Testing', () => {
     expect(newList.lists.length).toBe(1);
     expect(newList.lists[0].description).toBe('New Task 1');
   });
+  test('Remove Task', () => {
+    newList.deleteTask('btn-0');
+    expect(newList.lists.length).toBe(0);
+  });
+  test('Edit Task Description', () => {
+    taskInput.value = 'Task to be edited';
+    newList.addTasks();
+    newList.editTask('description-0', newList.lists[0].description);
+    taskInput.value = 'Edited Task';
+    newList.addTasks();
+    expect(newList.lists[0].description).toBe('Edited Task');
+  });
 });
